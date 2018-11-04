5cccd938f89c06cccf9c0f99b5e6bcf8940f3656
mariolarumlova <mariolarumlova@gmail.com>
2018-11-04 21:11:36 +0100

    [FAILED] Filtering by various dates, 2 params in methods

--- src/main/java/pl/mrumlova/autodrom/repository/MessageRepository.java
+++ src/main/java/pl/mrumlova/autodrom/repository/MessageRepository.java
@@ -2,6 +2,7 @@ package pl.mrumlova.autodrom.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.mrumlova.autodrom.model.Message;
 
 import java.util.List;
@@ -20,4 +21,19 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
     @Query("select m from Message m order by m.saveDate")
     List<Message> orderBySaveDate();
 
+    @Query("select m from Message m where str_to_date(m.beginDate, %Y-%m-%d)>=str_to_date(borderFrom, %Y-%m-%d) " +
+            "and str_to_date(m.endDate, %Y-%m-%d)<=str_to_date(borderTo, %Y-%m-%d)")
+    List<Message> getMessagesWhereBeginDateIs(@Param("borderFrom") String borderFrom, @Param("borderTo") String borderTo);
+
+    @Query("select m from Message m where str_to_date(m.endDate, %Y-%m-%d)>=str_to_date(borderFrom, %Y-%m-%d) " +
+            "and str_to_date(m.endDate, %Y-%m-%d)<=str_to_date(borderTo, %Y-%m-%d)")
+    List<Message> getMessagesWhereEndDateIs(@Param("borderFrom") String borderFrom, @Param("borderTo") String borderTo);
+
+    @Query("select m from Message m where str_to_date(m.saveDate, %Y-%m-%d)>=str_to_date(borderFrom, %Y-%m-%d) " +
+            "and str_to_date(m.endDate, %Y-%m-%d)<=str_to_date(borderTo, %Y-%m-%d)")
+    List<Message> getMessagesWhereSaveDateIs(@Param("borderFrom") String borderFrom, @Param("borderTo") String borderTo);
+
+    @Query("select m from Message m where str_to_date(m.updateDate, %Y-%m-%d)>=str_to_date(borderFrom, %Y-%m-%d) " +
+            "and str_to_date(m.endDate, %Y-%m-%d)<=str_to_date(borderTo, %Y-%m-%d)")
+    List<Message> getMessagesWhereUpdateDateIs(@Param("borderFrom") String borderFrom, @Param("borderTo") String borderTo);
 }
